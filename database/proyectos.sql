6f331fee12aaac0f65807ea4bccd74d52237aa74
Jose R Flores Q <46694145+JoseRFloresQ@users.noreply.github.com>
2021-01-15 18:43:03 +0100

    Añadido sql para insercion datos
    Se han incluido sql para insertar empresas, proyectos, y relacionar esas empresas con dichos proyectos

---
+++ database/proyectos.sql
@@ -0,0 +1,6 @@
+INSERT INTO poyectos VALUES (1, '2021-01-2 11:00:00', '2021-01-18 20:00:00', 'Descripcion del proyecto numero 1'),
+(2, '2021-01-2 10:00:00', '2021-01-8 19:00:00', 'Descripcion del proyecto numero 2'),
+(3, '2021-01-6 10:00:00', '2021-01-16 21:00:00', 'Descripcion del proyecto numero 3'),
+(4, '2021-01-1 11:00:00', '2021-01-14 20:00:00', 'Descripcion del proyecto numero 4'),
+(5, '2021-01-6 12:00:00', '2021-01-13 21:00:00', 'Descripcion del proyecto numero 5'),
+(6, '2021-01-3 11:00:00', '2021-01-22 21:00:00', 'Descripcion del proyecto numero 6');
