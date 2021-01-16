67c5de45eb3a90be77c3b667a55559e195cb790f
jaimegonzalezcarcam <jaime.gonzalezcarcam@edu.uah.es>
2021-01-16 23:56:53 +0100

    fichar horas

--- IngenierosPeso/src/servlets/ProyectoServlet.java
+++ IngenierosPeso/src/servlets/ProyectoServlet.java
@@ -1,7 +1,5 @@
 package servlets;
 
-
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -10,13 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import util.ProyectoDao;
+
 @WebServlet("/ProyectoServlet")
 public class ProyectoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     public ProyectoServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -25,6 +24,14 @@ public class ProyectoServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+		String idProy = request.getParameter("idProy");
+        String aux = request.getParameter("horasDedicadas");	
+        int horasDedicadas = Integer.parseInt(aux);
+        String dni = (String) request.getAttribute("dni");
+        ProyectoDao dao = new ProyectoDao();
+        dao.updateHoras(dni, idProy, horasDedicadas);
+        request.setAttribute("dni", dni); 
+        request.getRequestDispatcher("/ficharHoras.jsp").forward(request, response);
 	}
 
 }
